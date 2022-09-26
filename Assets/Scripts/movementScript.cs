79be08a8d6fc9a2527be255ad13264afb1228cff
Ed-Dijkstra <ed.dijkstra2@gmail.com>
2022-09-26 13:41:19 +0200

    moveSpeed variable
    made a variable to control the speed of movement. Also added various comments to movementScript.

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -8,25 +8,31 @@ public class movementScript : MonoBehaviour
     List<InputDevice> leftHandedControllers = new List<InputDevice>();
     Vector2 triggerValue;
     GameObject cameraObject;
+    [Tooltip("Speed of movement. 1 is really fast.")]
+    [SerializeField]
+    private float moveSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
+        //Stores all left-hand controllers in a list.
         var desiredCharacteristics = InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, leftHandedControllers);
-
         foreach (var device in leftHandedControllers)
         {
             Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", device.name, device.characteristics.ToString()));
         }
-        //Camera object is the child of this object's child.
+        //Camera object is the child of this object's child. Stored to use its rotation value later
         cameraObject = transform.GetChild(0).GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get the first entry of the left-hand controller list and checks the input value of its joystick.
         if (leftHandedControllers[0].TryGetFeatureValue(CommonUsages.primary2DAxis, out triggerValue) && !triggerValue.Equals(new Vector2(0, 0)))
         {
+            triggerValue *= moveSpeed;
+            //Moves the object in the direction the joystick is pressed relative to the camera rotation.
             transform.Translate(new Vector3(triggerValue.x, 0, triggerValue.y), cameraObject.transform);
         }
     }
