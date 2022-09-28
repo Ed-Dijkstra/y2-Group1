e5200f06c18f04574515e034454108656dbc9fe8
Ed Dijkstra <e.h.dijkstra.2@student.rug.nl>
2022-09-28 11:38:08 +0200

    Removed rotation when holding object.
    The right joystick will now only be used to move an object if you're holding one, and not to look around.
    Also changed movement type of held object so you can no longer move it through the floor, wall or other objects while holding it.

--- Assets/Scripts/rotateScript.cs
+++ Assets/Scripts/rotateScript.cs
@@ -8,6 +8,7 @@ public class rotateScript : MonoBehaviour
     // List to store the right-hand controller
     private List<InputDevice> rightHandedControllers = new List<InputDevice>();
     private Vector2 triggerValue;
+    private bool gripValue;
     [SerializeField]
     private float rotateSpeed = 0.1f;
     // Start is called before the first frame update
@@ -25,14 +26,18 @@ public class rotateScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get the first entry of the right-hand controller list and checks the input value of its joystick.
-        if (rightHandedControllers[0].TryGetFeatureValue(CommonUsages.primary2DAxis, out triggerValue) && !triggerValue.Equals(new Vector2(0, 0)))
+        // Only run this (look around) if the grip button is not being pressed.
+        if (rightHandedControllers[0].TryGetFeatureValue(CommonUsages.gripButton, out gripValue) && !gripValue)
         {
-            // Modiefies the y rotation of the camera if the right joystick is used (look left and right)
-            triggerValue.x *= rotateSpeed;
-            var currentRotation = transform.eulerAngles;
-            var newY = currentRotation.y + triggerValue.x;
-            transform.eulerAngles = new Vector3(currentRotation.x, newY, currentRotation.z);
+            // Get the first entry of the right-hand controller list and checks the input value of its joystick.
+            if (rightHandedControllers[0].TryGetFeatureValue(CommonUsages.primary2DAxis, out triggerValue) && !triggerValue.Equals(new Vector2(0, 0)))
+            {
+                // Modiefies the y rotation of the camera if the right joystick is used (look left and right)
+                triggerValue.x *= rotateSpeed;
+                var currentRotation = transform.eulerAngles;
+                var newY = currentRotation.y + triggerValue.x;
+                transform.eulerAngles = new Vector3(currentRotation.x, newY, currentRotation.z);
+            }
         }
     }
 }
