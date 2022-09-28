a2a0150a95ccbb9cc2ad7356252a9e15fa8afad2
Ed Dijkstra <e.h.dijkstra.2@student.rug.nl>
2022-09-28 11:56:05 +0200

    Removed movement while holding object with left hand

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -9,6 +9,7 @@ public class movementScript : MonoBehaviour
     private List<InputDevice> leftHandedControllers = new List<InputDevice>();
     private Vector2 triggerValue;
     private GameObject cameraObject;
+    private bool gripValue;
     [Tooltip("Speed of movement. 1 is really fast.")]
     [SerializeField]
     private float moveSpeed = 0.1f;
@@ -29,21 +30,24 @@ public class movementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get the first entry of the left-hand controller list and checks the input value of its joystick.
-        if (leftHandedControllers[0].TryGetFeatureValue(CommonUsages.primary2DAxis, out triggerValue) && !triggerValue.Equals(new Vector2(0, 0)))
+        if (leftHandedControllers[0].TryGetFeatureValue(CommonUsages.gripButton, out gripValue) && !gripValue)
         {
-            // modify the value so you don't go super fast
-            triggerValue *= moveSpeed;
-            var directionVector = new Vector3(triggerValue.x, 0, triggerValue.y);
-            // I make a temporary object to store the camera's rotation in, but I remove the Y component so it doesn't let you fly.
-            var tempValues = cameraObject.transform.eulerAngles;
-            tempValues.x = 0;
-            var tempObj = Instantiate(new GameObject());
-            tempObj.transform.eulerAngles = tempValues;
-            // Moves the object in the direction the joystick is pressed relative to the camera rotation.
-            transform.Translate(directionVector, tempObj.transform);
-            // removes the temporary object.
-            Destroy(tempObj);
+            // Get the first entry of the left-hand controller list and checks the input value of its joystick.
+            if (leftHandedControllers[0].TryGetFeatureValue(CommonUsages.primary2DAxis, out triggerValue) && !triggerValue.Equals(new Vector2(0, 0)))
+            {
+                // modify the value so you don't go super fast
+                triggerValue *= moveSpeed;
+                var directionVector = new Vector3(triggerValue.x, 0, triggerValue.y);
+                // I make a temporary object to store the camera's rotation in, but I remove the Y component so it doesn't let you fly.
+                var tempValues = cameraObject.transform.eulerAngles;
+                tempValues.x = 0;
+                var tempObj = Instantiate(new GameObject());
+                tempObj.transform.eulerAngles = tempValues;
+                // Moves the object in the direction the joystick is pressed relative to the camera rotation.
+                transform.Translate(directionVector, tempObj.transform);
+                // removes the temporary object.
+                Destroy(tempObj);
+            }
         }
     }
 }
