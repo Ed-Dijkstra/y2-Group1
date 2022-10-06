3363ae603efae86d0c3c0b7ea19b54a3b5c0e648
Ed Dijkstra <e.h.dijkstra.2@student.rug.nl>
2022-10-06 14:15:42 +0200

    Fixed snap
    after testing, fixed the snap functionality that now works.

--- Assets/Scripts/SnapScript.cs
+++ Assets/Scripts/SnapScript.cs
@@ -20,16 +20,16 @@ public class SnapScript : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
         // If the touching object has a Snappable component, and its ID is the same as this one's ID.
         // TODO: check for rotation so you have to rotate the object correctly before it snaps.
-        if (collision.gameObject.TryGetComponent<Snappable>(out snapObject) && snapObject.GetSnapId().Equals(snapId))
+        if (other.gameObject.TryGetComponent<Snappable>(out snapObject) && snapObject.GetSnapId().Equals(snapId))
         {
             // Activate this objects child (the snapped object
             transform.GetChild(0).gameObject.SetActive(true);
             // Remove the other object
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
