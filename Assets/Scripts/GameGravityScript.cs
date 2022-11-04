57bf8c576fe696762855188eae091ecec8307bc5
Ed-Dijkstra <ed.dijkstra2@gmail.com>
2022-11-04 19:18:37 +0100

    Added grab to crates and fuel tank

--- Assets/Scripts/GameGravityScript.cs
+++ Assets/Scripts/GameGravityScript.cs
@@ -29,6 +29,7 @@ public class GameGravityScript : MonoBehaviour
         // Enables gravity on the player and in this object's boolean if it is not enalbed already.
         if (!gravity)
         {
+            Physics.gravity = new Vector3(0f, 9.81f, 0f);
             gravity = true;
         }
     }
@@ -38,6 +39,7 @@ public class GameGravityScript : MonoBehaviour
         // Disables gravity on the player and in this object's boolean if it is not disabled already.
         if (gravity)
         {
+            Physics.gravity = Vector3.zero;
             gravity = false;
         }
     }
