9d8b6f8bb43e8d86c8197349f45bf0ba89791524
Ed-Dijkstra <ed.dijkstra2@gmail.com>
2022-11-05 13:54:45 +0100

    Fixed occlusion culling
    Also added some debug stuff that is not intended to feature in the game itself

---
+++ Assets/Scripts/Debug/FPS.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    Text fpsText;
+    public int refreshRate = 10;
+    int frameCounter;
+    float totalTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsText = GetComponent<Text>();
+        frameCounter = 0;
+        totalTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (frameCounter == refreshRate)
+        {
+            float averageFPS = (1.0f / (totalTime / refreshRate));
+            fpsText.text = averageFPS.ToString("F1");
+            frameCounter = 0;
+            totalTime = 0;
+        } else
+        {
+            totalTime += Time.deltaTime;
+            frameCounter++;
+        }
+    }
+}
